d6b7e89efee3838100df0537ec11b306f1b681da
Atsuto0519 <atsuto.fps@gmail.com>
2018-10-28 22:18:38 +0900

    Add process file

---
+++ CatBoost/train_sushi.py
@@ -0,0 +1,36 @@
+import pickle
+import pandas as pd
+from catboost import *
+from sklearn.model_selection import train_test_split
+
+train_path = "./data/sushi3-2016/train.csv"
+model_path = "./model.pkl"
+
+df = pd.read_table(train_path,sep=' ',header=None)
+df = df.rename(columns={0: 'user_id', 19: 'score'})
+
+# train_df, test_df = train_test_split(train_df, test_size=0.3)
+train_df = df[1:1000]
+test_df = df[1000:1010]
+
+
+cat_features = list(range(12))
+
+train_data = train_df.drop(["user_id", "score"], axis=1)
+train_label = train_df["score"]
+train_group_id = train_df["user_id"]
+train_pool = Pool(data=train_data, label=train_label, group_id=train_group_id, cat_features=cat_features)
+
+test_data = test_df.drop(["user_id", "score"], axis=1)
+test_label = test_df["score"]
+test_group_id = test_df["user_id"]
+test_pool = Pool(data=test_data, label=test_label, group_id=test_group_id, cat_features=cat_features)
+
+# YetiRankはpairwiseでのランク学習の1つ
+param = {'loss_function':'YetiRank', 'learning_rate': .05, 'iterations': 200,
+         'depth': 4, 'use_best_model':True}
+model = CatBoost(param)
+model.fit(train_pool, eval_set=test_pool)
+
+with open(model_path, mode='wb') as f:
+    pickle.dump(model, f)
