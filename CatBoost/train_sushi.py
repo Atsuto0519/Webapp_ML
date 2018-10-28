468d086ac6cdffc72d7a9c46fb187f2572efb4e2
Atsuto0519 <atsuto.fps@gmail.com>
2018-10-28 22:27:38 +0900

    Fix datasets again and again

--- CatBoost/train_sushi.py
+++ CatBoost/train_sushi.py
@@ -9,8 +9,8 @@ model_path = "./model.pkl"
 df = pd.read_table(train_path,sep=' ',header=None)
 df = df.rename(columns={0: 'user_id', 19: 'score'})
 
-train_df = df[1:len(df)-100]
-test_df = df[len(df)-100:len(df)]
+train_df = df[1:len(df)-10]
+test_df = df[len(df)-10:len(df)]
 
 
 cat_features = list(range(12))
